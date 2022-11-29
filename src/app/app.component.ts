32ea58278d67cbf9e4aeffc1c47f15ea218e67ac
User <user@Users-MacBook.local>
2022-11-29 08:14:42 +0200

    Committer: chavdar <chavdar@Users-MacBook.local>  On branch master  Your branch is up-to-date with 'origin/master'.  Changes to be committed: 	modified:   src/app/app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'angular-video-series';
+  intervalSub: string | number | NodeJS.Timer | undefined;
+  
+  ngOnInit() {
+    this.intervalSub = setInterval(()=>{
+      console.log('Hello from ngOnInit');
+
+    }, 1000);
+  }
+
+  ngOnDestroy() {
+    if(this.intervalSub) {
+      clearInterval(this.intervalSub);
+
+    }
+  }
 }
